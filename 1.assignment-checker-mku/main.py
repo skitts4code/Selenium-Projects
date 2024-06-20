594275e9b5fc285ea9640bb35f0d04b43d8f7d60
skitts4code <skitts4code@proton.me>
2024-06-20 13:12:41 +0300

    add list of courses

--- 1.assignment-checker-mku/main.py
+++ 1.assignment-checker-mku/main.py
@@ -7,24 +7,25 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 
 # path to chromedriver
-service = Service(executable_path="/home/skt/webdriver/chromedriver")
+service = Service(executable_path="./chromedriver")
 
 
 driver = webdriver.Chrome(service=service)
 # webpage to access
 driver.get("https://elearning.mku.ac.ke/portal")
 
+
 # wait for 10 seconds
 WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.NAME, "eid"))
 )
 
-reg_no = input("Enter your registration number: ")
-pass_word = input("Enter your password: ")
+# reg_no = input("Enter your registration number: ")
+# pass_word = input("Enter your password: ")
 
 user_name = driver.find_element(By.NAME, "eid")
 user_name.clear()
-user_name.send_keys(reg_no + Keys.ENTER )
+user_name.send_keys("regno" + Keys.ENTER )
 
 WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.NAME, "eid"))
@@ -32,29 +33,27 @@ WebDriverWait(driver, 10).until(
 
 pass_word = driver.find_element(By.NAME, "pw")
 pass_word.clear()
-pass_word.send_keys(pass_word + Keys.ENTER )
+pass_word.send_keys("password" + Keys.ENTER )
 
-WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'BBM4104')]"))
-)
+course_codes = ["BIT3201", "BIT2205", "BIT2204", "BIT2103", "BEG2112", "BIT3209", "BIT3102", "BIT3202"]
 
-course_code = driver.find_element(By.XPATH, "//*[contains(text(), 'BBM4104')]")
-course_code.click()
+for course in course_codes:
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, f"//*[contains(text(), '{course}')]"))
+    )
 
-
-# WebDriverWait(driver, 10).until(
-#     EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'Assignments')]"))
-# )
-
-course_item = driver.find_element(By.XPATH, "//*[contains(text(), 'Assignments')]")
-course_item.click()
-
-driver.save_screenshot("./pics/assignment1.png")
+    course_code = driver.find_element(By.XPATH, f"//*[contains(text(), '{course}')]")
+    course_code.click()
 
 
-driver.back()
+    course_item = driver.find_element(By.XPATH, "//*[contains(text(), 'Assignments')]")
+    course_item.click()
 
-time.sleep(5)
+    driver.save_screenshot(f"./pics/{course}_assignment.png")
+
+    driver.back()
+
+    time.sleep(5)
 
 #Check for Tests and Quizzes
 WebDriverWait(driver, 10).until(
