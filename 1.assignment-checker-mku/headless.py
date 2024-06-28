d28d6a9d633955318d0b91aceb9e8c38c78b34a2
skittsforcode <118190876+skitts4code@users.noreply.github.com>
2024-06-28 08:27:51 +0300

    working headless mode!!!
    sheesh

---
+++ 1.assignment-checker-mku/headless.py
@@ -0,0 +1,81 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+import time
+
+def login(driver, reg_no, pass_word):
+    user_name = driver.find_element(By.NAME, "eid")
+    user_name.clear()
+    user_name.send_keys(reg_no + Keys.ENTER)
+
+    pass_word_field = driver.find_element(By.NAME, "pw")
+    pass_word_field.clear()
+    pass_word_field.send_keys(pass_word + Keys.ENTER)
+
+def check_course(driver, course):
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "Mrphs-toolsNav__menuitem--title"))
+    )
+
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, f"//*[contains(text(), '{course}')]"))
+    )
+
+    course_code = driver.find_element(By.XPATH, f"//*[contains(text(), '{course}')]")
+    course_code.click()
+
+    course_item = driver.find_element(By.XPATH, "//*[contains(text(), 'Assignments')]")
+    course_item.click()
+
+    driver.save_screenshot(f"./pics/{course}_assignment.png")
+
+    driver.back()
+
+    time.sleep(5)
+
+def main():
+    # path to chromedriver
+    service = Service(executable_path="./chromedriver.exe")
+
+    chrome_options = Options()  
+    chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--window-size=1920,1080")
+
+    driver = webdriver.Chrome(options=chrome_options)
+    # webpage to access
+    driver.get("https://elearning.mku.ac.ke/portal")
+
+    reg_no = input("Enter your registration number: ")
+    pass_word = input("Enter your password: ")
+
+    login(driver, reg_no, pass_word)
+
+    course_codes = ["BIT3201", "BIT2205", "BIT2204", "BIT2103", "BEG2112", "BIT3209", "BIT3102", "BIT3202"]
+
+    for course in course_codes:
+        check_course(driver, course)
+
+    # Check for Tests and Quizzes
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'Tests & Quizzes')]"))
+    )
+
+    tests = driver.find_element(By.XPATH, "//*[contains(text(), 'Tests & Quizzes')]")
+    tests.click()
+
+    time.sleep(5)
+
+    driver.save_screenshot("./pics/Tests.png")
+
+    time.sleep(100)
+
+    driver.quit()
+
+if __name__ == "__main__":
+    main()
